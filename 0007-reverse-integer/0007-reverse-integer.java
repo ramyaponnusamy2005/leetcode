cf2dfb42b1b4c08d1f7cfd6e7182f207dfe3f58b
ramyaponnusamy2005 <ramyaponnusamy2005@gmail.com>
2025-04-06 10:23:41 +0530

    Time: 125 ms (3.74%), Space: 40.7 MB (84.98%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -1,10 +1,10 @@
 class Solution {
     public int reverse(int x) {
         int result = 0;
-        boolean neg = false;
+        boolean flag = false;
         if(x == 0) return 0;
         if(x < 0){
-            neg = true;
+            flag = true;
             x *= -1;
         }
 
@@ -15,7 +15,7 @@ class Solution {
             x /= 10;
         }
 
-        if(!neg) return result;
+        if(!flag) return result;
         else return result * -1;
     }
 }
