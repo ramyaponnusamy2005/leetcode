bfb9714055c86bc22199b26749f12fdb3e52ac71
ramyaponnusamy2005 <ramyaponnusamy2005@gmail.com>
2026-05-23 16:04:30 +0530

    Time: 0 ms (100%), Space: 43.3 MB (59.88%) - LeetHub - Time: O(?), Space: O(?)

---
+++ 0066-plus-one/0066-plus-one.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        for(int i=digits.length-1;i>=0;i--){
+            if(digits[i]<9){
+                digits[i]++;
+                return digits;
+            }
+            digits[i]=0;
+        }
+        
+        int[]num=new int[digits.length+1];
+        num[0]=1;
+        return num;
+    }
+
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
